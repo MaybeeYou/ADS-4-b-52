157bc4811906b08cca69813ac67ea706ec989983
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-04-07 23:06:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,25 +12,24 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int fl = 0, point = len - 1;
-  for (int i = 0; i < len - 1; ++i) {
-    int j = point;
-    while (i < j) {
-      int sum = arr[i] + arr[j];
-      if (sum == value) {
-        count++;
-        if (fl == 1) {
-          point = j;
-          fl = 0;
-        }
-      } else if (sum < value) {
-        break;
-      } else {
-        fl = 1;
-      }
-      j--;
+  int left = 0;
+  int right = len - 1;
+
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      count++;
+      left++;
+      right--;
+    }
+    else if (sum < value) {
+      left++;
+    }
+    else {
+      right--;
     }
   }
+  return count;
 }
 int BinarySearch(int* arr, int left, int right, int value) {
   int first = -1, _left = left, _right = right;
