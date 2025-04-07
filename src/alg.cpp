3447a4f1f16987e3254670b0dc8a993ecff06d63
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-04-07 22:57:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,55 +14,46 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
-
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      count++;
-      left++;
-      right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
+      if (arr[left] == arr[right]) {
+        int n = right - left + 1;
+        count += n * (n - 1) / 2;
+        break;
+      }
+      int left_val = arr[left];
+      int right_val = arr[right];
+      int left_count = 1;
+      int right_count = 1;
+      while (left + 1 < right && arr[left + 1] == left_val) { left++; left_count++; }
+      while (right - 1 > left && arr[right - 1] == right_val) { right--; right_count++; }
+      count += left_count * right_count;
+      left++; right--;
     }
+    else if (sum < value) left++;
+    else right--;
   }
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; ++i) {
+  for (int i = 0; i < len - 1; i++) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
-    int first_pos = len;
-
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (arr[mid] >= target) {
-        first_pos = mid;
-        right = mid - 1;
-      } else {
-        left = mid + 1;
+      if (arr[mid] == target) {
+        count++;
+        int j = mid - 1;
+        while (j > i && arr[j] == target) { count++; j--; }
+        j = mid + 1;
+        while (j < len && arr[j] == target) { count++; j++; }
+        break;
       }
-    }
-
-    left = i + 1;
-    right = len - 1;
-    int last_pos = -1;
-
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] <= target) {
-        last_pos = mid;
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-
-    if (first_pos <= last_pos) {
-      count += (last_pos - first_pos + 1);
+      else if (arr[mid] < target) left = mid + 1;
+      else right = mid - 1;
     }
   }
   return count;
