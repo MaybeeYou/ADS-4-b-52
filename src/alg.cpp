2a19bb94ed8c77dd3a475be54aa50d10010891f9
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-04-07 23:04:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,39 +11,26 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-int count = 0;
-int left = 0;
-int right = len - 1;
-while (left < right) {
-  int sum = arr[left] + arr[right];
-  if (sum == value) {
-    if (arr[left] == arr[right]) {
-      int n = right - left + 1;
-      count += n * (n - 1) / 2;
-      break;
+  int count = 0;
+  int fl = 0, point = len - 1;
+  for (int i = 0; i < len - 1; ++i) {
+    int j = point;
+    while (i < j) {
+      int sum = arr[i] + arr[j];
+      if (sum == value) {
+        count++;
+        if (fl == 1) {
+          point = j;
+          fl = 0;
+        }
+      } else if (sum < value) {
+        break;
+      } else {
+        fl = 1;
+      }
+      j--;
     }
-    int left_val = arr[left];
-    int right_val = arr[right];
-    int left_count = 1;
-    int right_count = 1;
-    while (left + 1 < right && arr[left + 1] == left_val) {
-      left++;
-      left_count++;
-    }
-    while (right - 1 > left && arr[right - 1] == right_val) {
-      right--;
-      right_count++;
-    }
-    count += left_count * right_count;
-    left++;
-    right--;
-  } else if (sum < value) {
-    left++;
-  } else {
-    right--;
   }
-}
-return count;
 }
 int BinarySearch(int* arr, int left, int right, int value) {
   int first = -1, _left = left, _right = right;
