ccb3debf434b0fdd6c9424c280cc3619d193bce3
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-04-07 22:33:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,43 +2,34 @@
 int findParis(int *arr, int value, int n, int l, int r) {
   int count = 0;
   int middle = (l + r) / 2;
-
   int l_indx = -1;
   int r_indx = -1;
-
   if (r < l) {
     return 0;
   } else if (n + arr[middle] == value) {
     count++;
-
     if (middle - 1 >= l) {
       l_indx = middle - 1;
     }
-
     if (middle + 1 <= r) {
       r_indx = middle + 1;
     }
-
     while (l_indx >= l && n + arr[l_indx] == value) {
       count++;
       l_indx--;
     }
-
     while (r_indx <= r && r_indx != -1 && n + arr[r_indx] == value) {
       count++;
       r_indx++;
     }
-
     return count;
   } else {
     if (n + arr[middle] < value) {
       return findParis(arr, value, n, middle + 1, r);
     }
-
     return findParis(arr, value, n, l, middle - 1);
   }
 }
-
 int countPairs1(const int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; ++i) {
@@ -48,7 +39,6 @@ int countPairs1(const int *arr, int len, int value) {
       }
     }
   }
-
   return count;
 }
 
@@ -71,7 +61,6 @@ int countPairs2(const int *arr, int len, int value) {
       }
     }
   }
-
   return count;
 }
 
@@ -81,7 +70,6 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; ++i) {
     count += findParis(arr, value, arr[i], i + 1, len - 1);
   }
-
   return count;
 }
 
